cd904b80e9519d81024c998404ba859c305f3d7a
kingsley-shiu <shiukingpan@gmail.com>
2022-01-16 22:58:36 +0800

    add sg mso

--- get_lic_co.py
+++ get_lic_co.py
@@ -17,13 +17,40 @@ def get_hk_mso():
     mso = r.content
 
     df_mso = pd.DataFrame(json.loads(mso.decode('utf8'))['pubSrchList'])
-    df_mso = df_mso[['coName', 'coNameChn', 'licExprDt', 'licNum']].drop_duplicates()
+    df_mso = df_mso.drop_duplicates(['coName', 'coNameChn'])
+    df_mso = df_mso.rename(columns={'coName': 'mso_name', 'coNameChn': 'mso_name_cn'})
+    df_mso['cntry_code'] = 'HK'
+    df_mso = df_mso[['cntry_code', 'mso_name', 'mso_name_cn']]
 
     return df_mso
 
 
-df_hk_mso = get_hk_mso()
+# %%
+def get_sg_mso():
+
+    session = HTMLSession()
+    sg_mso_url = "https://eservices.mas.gov.sg/fid/custom/printpartial"
+    r = session.get(sg_mso_url)
+
+    df_mso = pd.read_html(r.text)[0]
+
+    mso_cond = (df_mso['Licence Type/Status'].isin(['Money-changing Licensee']) |
+                df_mso['Activity/Business Type'].isin(['Money-changing Service',
+                                                       'Cross-border Money Transfer Service',
+                                                       'Domestic Money Transfer Service',
+                                                       'E-money Issuance Service', ])
+                )
+    df_mso = df_mso.drop_duplicates(['Organisation Name'])
+    df_mso = df_mso[mso_cond].rename(columns={'Organisation Name': 'mso_name'})
+    df_mso['cntry_code'] = 'SG'
+    df_mso = df_mso[['cntry_code', 'mso_name']]
+
+    return df_mso
+
 
 # %%
+df_hk_mso = get_hk_mso()
+df_sg_mso = get_sg_mso()
+df_mso = pd.concat([df_hk_mso, df_sg_mso])
 
-sg_mso_url = "https://eservices.mas.gov.sg/fid/institution/print"
+# %%
